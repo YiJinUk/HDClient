2893675b36758b7c46f7ed305995f6c349f5d016
yju123727 <yju123727@gmail.com>
2022-03-01 06:31:19 +0900

    - init

---
+++ Source/HDClient/HDClientGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HDClientGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HDCLIENT_API AHDClientGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
